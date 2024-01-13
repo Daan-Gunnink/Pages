0a2895270c492edc409c1365565afbc0c65e67c8
Daan Gunnink <github@kreukelz.one>
2024-01-13 20:49:06 +0100

    Add header

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,23 @@
 import { defineConfig } from 'astro/config';
 import storyblok from '@storyblok/astro';
 import { loadEnv } from 'vite';
-
 import vercel from '@astrojs/vercel/serverless';
-
+import tailwind from "@astrojs/tailwind";
 const env = loadEnv("", process.cwd(), 'STORYBLOK');
 
+// https://astro.build/config
 export default defineConfig({
-  integrations: [
-    storyblok({
-      accessToken: env.STORYBLOK_TOKEN,
-      components: {
-        blogPost: 'storyblok/BlogPost',
-        page: 'storyblok/Page',
-        blogPostList: 'storyblok/BlogPostList'
-      },
-    })
-  ],
+  integrations: [storyblok({
+    accessToken: env.STORYBLOK_TOKEN,
+    components: {
+      config: 'storyblok/Config',
+      blogPost: 'storyblok/BlogPost',
+      screenFillingImage: 'storyblok/ScreenFillingImage',
+      page: 'storyblok/Page',
+      blogPostList: 'storyblok/BlogPostList',
+      tiledGallery: 'storyblok/TiledGallery'
+    }
+  }), tailwind()],
   output: 'server',
-  adapter: vercel(),
+  adapter: vercel()
 });
