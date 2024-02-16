60c288537e4002d643c6f4733fc86835cf07befc
Daan Gunnink <github@kreukelz.one>
2024-02-16 17:50:55 +0100

    Implement floating mobile menu

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,25 +3,24 @@ import storyblok from "@storyblok/astro";
 import { loadEnv } from "vite";
 import vercel from "@astrojs/vercel/serverless";
 import tailwind from "@astrojs/tailwind";
+import icon from "astro-icon";
 const env = loadEnv("", process.cwd(), "STORYBLOK");
+
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    storyblok({
-      accessToken: env.STORYBLOK_TOKEN,
-      components: {
-        config: "storyblok/Config",
-        blogPost: "storyblok/BlogPost",
-        screenFillingImage: "storyblok/ScreenFillingImage",
-        text: "storyblok/Text",
-        page: "storyblok/Page",
-        blogPostList: "storyblok/BlogPostList",
-        tiledGallery: "storyblok/TiledGallery",
-      },
-    }),
-    tailwind(),
-  ],
+  integrations: [storyblok({
+    accessToken: env.STORYBLOK_TOKEN,
+    components: {
+      config: "storyblok/Config",
+      blogPost: "storyblok/BlogPost",
+      screenFillingImage: "storyblok/ScreenFillingImage",
+      text: "storyblok/Text",
+      page: "storyblok/Page",
+      blogPostList: "storyblok/BlogPostList",
+      tiledGallery: "storyblok/TiledGallery"
+    }
+  }), tailwind(), icon()],
   output: "server",
-  adapter: vercel(),
+  adapter: vercel()
 });
