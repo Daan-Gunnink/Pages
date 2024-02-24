b1ad15b603d62ce49d998fb4eb25e13da1610aef
Daan Gunnink <github@kreukelz.one>
2024-02-24 18:01:49 +0100

    Implement Gallery

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -8,7 +8,21 @@ export default {
 			pattern: /grid-cols-./,
 			variants: ['lg', 'md']
 		},
-	  ],
+		'col-span-full',
+		'col-span-1',
+		'w-1/2',
+		'w-1/3',
+		'w-1/4',
+		'text-left',
+		'text-center',
+		'text-right',
+		'justify-self-start',
+		'justify-self-center',
+		'justify-self-end',
+		'self-start',
+		'self-center',
+		'self-end'
+	],
 	theme: {},
 	plugins: [],
 }
